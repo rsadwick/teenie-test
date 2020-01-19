77c44f6d462accd74bf45d3d9bd969ec56da71a9
Ryan Sadwick <rsadwick@gmail.com>
2020-01-19 18:39:12 -0500

    push

---
+++ _next/static/utMVHha9ao_zesckLO4u3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/bag":[a],"/confirmation":[a],"/description":[a],"/index":[a],"/products":[a]}}("static\u002Fchunks\u002Fddce6f38d8c2d0d3658d0be6991c7212aae1c410.5047d15ba444687d5dbd.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
